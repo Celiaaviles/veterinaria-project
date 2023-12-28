fa6580b04c2cad8ea8f5995179c0a51cf0f6ebde
Celia Avilés <celiavicaviles@gmail.com>
2023-12-28 23:52:31 +0100

    Add header with styles

--- src/app.jsx
+++ src/app.jsx
@@ -1,10 +1,14 @@
+import Formulario from "./components/formulario";
 import Header from "./components/header";
+import ListadoPacientes from "./components/listadoPacientes";
 
 function App() {
   return (
-    <>
+    <div className="container mx-auto mt-20">
       <Header></Header>
-    </>
+      <Formulario></Formulario>
+      <ListadoPacientes></ListadoPacientes>
+    </div>
   );
 }
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,8 +1,11 @@
 function Header() {
   return (
-    <div className="header">
-      <h1>Header</h1>
-    </div>
+    <>
+      <h1 className="font-black text-5xl text-center md:w-2/3 mx-auto ">
+        Seguimiento Pacientes {""}
+        <span className="text-indigo-600">Veterinaria</span>
+      </h1>
+    </>
   );
 }
 
