4e91abcab5dceb79a23fd1c56b4a6f919d4c86e1
Celia Avilés <celiavicaviles@gmail.com>
2023-12-29 11:23:40 +0100

    Add form with styles

--- src/app.jsx
+++ src/app.jsx
@@ -6,8 +6,10 @@ function App() {
   return (
     <div className="container mx-auto mt-20">
       <Header></Header>
-      <Formulario></Formulario>
-      <ListadoPacientes></ListadoPacientes>
+      <div className="mt-12 md:flex">
+        <Formulario></Formulario>
+        <ListadoPacientes></ListadoPacientes>
+      </div>
     </div>
   );
 }
--- src/components/listadoPacientes.jsx
+++ src/components/listadoPacientes.jsx
@@ -1,7 +1,7 @@
 export default function ListadoPacientes() {
   return (
-    <>
+    <div className="w-1/2 lg:w-3/5">
       <h2>Listado Pacientes</h2>
-    </>
+    </div>
   );
 }
