dffeca9d084defaf5d4d997c6703821da54e6621
Celia Avilés <celiavicaviles@gmail.com>
2023-12-29 11:40:28 +0100

    Add Paciente component with styles

---
+++ src/components/paciente.jsx
@@ -0,0 +1,35 @@
+export default function Paciente() {
+  return (
+    <div className="m-3 bg-white shadow-md px-5 py-10 rounded-xl">
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case"></span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Nombre: {""}
+        <span className="font-normal normal-case">Vespa</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Propietario: {""}
+        <span className="font-normal normal-case">Celia</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        E-mail: {""}
+        <span className="font-normal normal-case">correo@correo.com</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Fecha Alta: {""}
+        <span className="font-normal normal-case">10 Diciembre 2023</span>
+      </p>
+      <p className="font-bold mb-3 text-gray-700 uppercase">
+        Síntomas: {""}
+        <span className="font-normal normal-case">
+          Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren
+          ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum
+          Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren ipsum Loren
+          ipsum Loren ipsum Loren ipsum{" "}
+        </span>
+      </p>
+    </div>
+  );
+}
