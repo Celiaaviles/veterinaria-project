35c4da049aafcf06369f42774f3af7bf195e8733
Celia Avilés <celiavicaviles@gmail.com>
2023-12-29 18:14:30 +0100

    Add conditional rendering to listadoPacientes

--- src/components/listadoPacientes.jsx
+++ src/components/listadoPacientes.jsx
@@ -4,14 +4,28 @@ import Paciente from "./paciente";
 export default function ListadoPacientes({ pacientes }) {
   return (
     <div className="md:w-1/2 lg:w-3/5 md:h-screen overflow-y-scroll">
-      <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
-      <p className="text-lg mt-5 text-center mb-10">
-        Administra tus {""}
-        <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
-      </p>
-      {pacientes?.map((paciente) => {
-        <Paciente key={paciente.id} paciente={paciente} />;
-      })}
+      {pacientes && pacientes.lenght ? (
+        <>
+          <h2 className="font-black text-3xl text-center">Listado Pacientes</h2>
+          <p className="text-lg mt-5 text-center mb-10">
+            Administra tus {""}
+            <span className="text-indigo-600 font-bold">Pacientes y Citas</span>
+          </p>
+          {pacientes.map((paciente) => (
+            <Paciente key={paciente.id} paciente={paciente} />
+          ))}
+        </>
+      ) : (
+        <>
+          <h2 className="font-black text-3xl text-center">No hay Pacientes</h2>
+          <p className="text-lg mt-5 text-center mb-10">
+            Comienza agregando Pacientes {""}
+            <span className="text-indigo-600 font-bold">
+              y aparecerán en este lugar
+            </span>
+          </p>
+        </>
+      )}
     </div>
   );
 }
