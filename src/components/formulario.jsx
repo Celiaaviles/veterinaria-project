f186670354cf1e87fcb44c0c9b4aac94f0e9b45a
Celia Avilés <celiavicaviles@gmail.com>
2023-12-30 14:33:30 +0100

    Add changes to deploy

--- src/components/formulario.jsx
+++ src/components/formulario.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import { useEffect, useState } from "react";
-import Error from "./Error";
+import Error from "./error";
 
 export default function Formulario({
   pacientes,
